cac184dde309c4f678b4eab10672cfd329e390af
Abner <1228428816@qq.com>
2024-08-01 13:00:20 +0800

    路由配置

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Layout =() => {
+    return (
+        <div>
+            <Outlet />
+            我是layout
+        </div>
+    )
+}
+
+export default Layout;
